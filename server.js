e8c92707c5d11c2046b966ef805e9058e2074bb1
NehaBajjuri <105218064+NehaBajjuri@users.noreply.github.com>
2023-06-15 17:18:11 +0530

    change

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 const app = express();
 const upload = multer({ dest: 'uploads/' });
 
-// app.use(express.static('public')); // Serve static files from the "public" directory
-app.use(express.static('InkedInsights', { index: 'create.html' }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'InkedInsights')));
+
 app.post('/process-post', upload.single('image'), (req, res) => {
   // Process the form data here
   const { title, category, author, bio, content } = req.body;
@@ -20,6 +21,6 @@ app.post('/process-post', upload.single('image'), (req, res) => {
   res.send('Post created successfully');
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(4000, () => {
+  console.log('Server is running on port 4000');
 });
