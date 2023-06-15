45d48c4b61299cccdb65b9f24e9661dde335afaf
NehaBajjuri <105218064+NehaBajjuri@users.noreply.github.com>
2023-06-15 22:28:06 +0530

    changes

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
+const mysql = require('mysql');
 
 const app = express();
 const upload = multer({ dest: 'uploads/' });
+
+// MySQL database connection configuration
+const connection = mysql.createConnection({
+  host: 'localhost', // replace with your database host
+  user: 'root', // replace with your database username
+  password: 'password', // replace with your database password
+  database: 'project' // replace with your database name
+});
+
+// Establish MySQL connection
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+  console.log('Connected to the database');
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,12 +33,28 @@ app.post('/process-post', upload.single('image'), (req, res) => {
   const { title, category, author, bio, content } = req.body;
   const image = req.file;
 
-  // Perform further processing (e.g., save to database, etc.)
+  // Save data to the database
+  const postData = {
+    title: title,
+    category: category,
+    author: author,
+    bio: bio,
+    content: content,
+    image: image.filename // assuming you're storing the filename in the database
+  };
 
-  // Redirect or send response
-  res.send('Post created successfully');
+  connection.query('INSERT INTO posts SET ?', postData, (err, results) => {
+    if (err) {
+      console.error('Error saving data to the database:', err);
+      res.status(500).send('Error saving data');
+      return;
+    }
+
+    // Redirect or send response
+    res.send('Post created successfully');
+  });
 });
 
-app.listen(4000, () => {
-  console.log('Server is running on port 4000');
+app.listen(5000, () => {
+  console.log('Server is running on port 5000');
 });
